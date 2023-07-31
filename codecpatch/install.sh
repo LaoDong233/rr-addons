0b909b4f267db3baf69c1035e66222fedf7f8ae3
Ing <wjz304@qq.com>
2023-07-31 16:44:01 +0800

    update codecpatch

--- codecpatch/install.sh
+++ codecpatch/install.sh
@@ -14,7 +14,7 @@ if [ "${1}" = "late" ]; then
   echo "[Service]"                           >>${DEST}
   echo "Type=oneshot"                        >>${DEST}
   echo "RemainAfterExit=true"                >>${DEST}
-  echo "ExecStart=/usr/bin/codecpatch.sh"    >>${DEST}
+  echo "ExecStart=/usr/bin/codecpatch.sh -p" >>${DEST}
   echo                                       >>${DEST}
   echo "[Install]"                           >>${DEST}
   echo "WantedBy=multi-user.target"          >>${DEST}
