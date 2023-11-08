601b1f9c3f68918f9856f20659f89d7c9c24c4bd
Ing <wjz304@qq.com>
2023-11-08 21:32:17 +0800

    fix wireless

--- wireless/install.sh
+++ wireless/install.sh
@@ -9,15 +9,23 @@
 if [ "${1}" = "late" ]; then
   echo "Installing daemon for wireless"
 
+  cp -vf /usr/bin/wireless_supplicant.sh /tmpRoot/usr/bin
+  cp -vf /usr/sbin/iw /tmpRoot/usr/sbin
+
   if [ ! -f /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db ]; then
     echo "copy esynoscheduler.db"
     mkdir -p /tmpRoot/usr/syno/etc/esynoscheduler
     cp -vf /addons/esynoscheduler.db /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db
   fi
-  echo "insert wireless task to esynoscheduler.db"
+
   export LD_LIBRARY_PATH=/tmpRoot/bin:/tmpRoot/lib
-  /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db <<EOF
-DELETE FROM task WHERE task_name LIKE 'Wireless';
+
+  if echo "SELECT * FROM task;" | /tmpRoot/bin/sqlite3 /usr/syno/etc/esynoscheduler/esynoscheduler.db | grep -q "Wireless"; then
+    echo "wireless task already exists"
+  else
+    echo "insert wireless task to esynoscheduler.db"
+    /tmpRoot/bin/sqlite3 /tmpRoot/usr/syno/etc/esynoscheduler/esynoscheduler.db <<EOF
 INSERT INTO task VALUES('Wireless', '', 'bootup', '', 0, 0, 0, 0, '', 0, '/usr/bin/wireless_supplicant.sh "wlan0" "SSID" "PASSWD"', 'script', '{}', '', '', '{}', '{}');
 EOF
+  fi
 fi
